d9d73e419674f7292076bdf9288fd0cedc59091b
Salvador Palmiciano <c-spalmiciano@fourwindsinteractive.com>
2019-07-23 21:52:35 -0300

    agregamos template y lo populamos con html

---
+++ js/index.js
@@ -0,0 +1,23 @@
+// guardamos ruta de la imagen en una variable
+var notaImage = "/copa_america.662e4e32.webp";
+
+// obtenemos nuestro template
+var NotaTpl = document.getElementById("NotaTpl");
+
+// logeamos nuestro template
+console.log(NotaTpl);
+
+// creamos un elemento de tipo div
+var nota = document.createElement("div");
+
+// traemos html de nuestro template
+nota.innerHTML = NotaTpl.innerHTML;
+
+// actualizamos titulo
+nota.querySelector(".text-container__title").innerHTML = "mi nuevo titulo";
+
+// actualizamos imagen
+nota.querySelector("img").src = notaImage;
+
+// appendeamos nuestro nuevo elemto.
+document.querySelector(".nota-container").appendChild(nota);
